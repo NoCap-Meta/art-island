9dd54b65474645671d627b9b4879aff1c2de11ca
Dishant Sharma <dishu5570@gmail.com>
2023-03-05 00:23:35 +0530

    mobile

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,23 @@
 import { Hero, Featured, Browse, Journey, Footer } from "components"
+import { MagnetMedium } from 'pages/_app';
 
 const Home = () => {
   return (
     <div>
-      <Hero />
-      <Featured />
-      <Browse />
-      <Journey />
-      <Footer />
+      <div className="md:flex flex-col hidden">
+        <Hero />
+        <Featured />
+        <Browse />
+        <Journey />
+        <Footer />
+      </div>
+      <div className="md:hidden flex items-center bg-[#f5dfc2] w-[100vw] h-[100vh] justify-center">
+        <div className="w-[90vw]">
+          <p className={`${MagnetMedium.className} text-[24px] text-center`}>
+            This is a mobile version of the website. Please visit on a desktop
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
