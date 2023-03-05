5bed1ee7be4d4aeca745d0851b2d1511680ed9f1
Dishant Sharma <dishu5570@gmail.com>
2023-03-05 13:06:07 +0530

    mobile

--- pages/art-page.js
+++ pages/art-page.js
@@ -2,10 +2,19 @@ import { ArtPageHero, Details, ItemActivity } from 'components'
 
 const ArtPage = () => {
   return (
-    <div className='pb-[125px] bg-[#f5dfc2]'>
-      <ArtPageHero />
-      <Details />
-      <ItemActivity />
+    <div>
+      <div className='pb-[125px] bg-[#f5dfc2] md:flex flex-col hidden'>
+        <ArtPageHero />
+        <Details />
+        <ItemActivity />
+      </div>
+      <div className="md:hidden flex items-center bg-[#f5dfc2] w-[100vw] h-[100vh] justify-center">
+        <div className="w-[90vw]">
+          <p className={`${MagnetMedium.className} text-[24px] text-center`}>
+            This is a mobile version of the website. Please visit on a desktop
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
