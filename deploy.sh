9fa556be37380d3215ce843ca6d97440c2366cb7
Dishant Sharma <dishu5570@gmail.com>
2023-04-30 00:55:07 +0530

    admin panel

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
-cd /home/ubuntu/nocap.network
-git pull origin main
-npm i &&
-npm run build &&
-pm2 restart nocap
+echo "Starting deployment"
+cd /home/ubuntu/nocap.network || { echo "Error: Could not change directory"; exit 1; }
+echo "Pulling latest changes from Git"
+git pull origin main || { echo "Error: Could not pull latest changes"; exit 1; }
+echo "Installing dependencies"
+npm i || { echo "Error: Could not install dependencies"; exit 1; }
+echo "Building the project"
+npm run build || { echo "Error: Could not build the project"; exit 1; }
+echo "Restarting the server with PM2"
+pm2 restart nocap || { echo "Error: Could not restart the server"; exit 1; }
+echo "Deployment successful!"
+exit 0;
