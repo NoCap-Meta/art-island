eae0d1d76c0aebc1b2a6c9b5429e51d18bd3e786
Dishant Sharma <dishu5570@gmail.com>
2023-04-30 10:30:40 +0530

    admin panel

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
+
 echo "Starting deployment"
+
+# Install Node.js and npm
+curl -sL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
 cd /home/ubuntu/nocap.network || { echo "Error: Could not change directory"; exit 1; }
+
 echo "Pulling latest changes from Git"
 git pull origin main || { echo "Error: Could not pull latest changes"; exit 1; }
+
 echo "Installing dependencies"
-npm i || { echo "Error: Could not install dependencies"; exit 1; }
+npm install || { echo "Error: Could not install dependencies"; exit 1; }
+
 echo "Building the project"
 npm run build || { echo "Error: Could not build the project"; exit 1; }
+
 echo "Restarting the server with PM2"
 pm2 restart nocap || { echo "Error: Could not restart the server"; exit 1; }
+
 echo "Deployment successful!"
 exit 0;
